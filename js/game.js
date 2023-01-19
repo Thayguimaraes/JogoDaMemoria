9717e06dba59da4458576936d9bf049172a5d8ec
Thaynara Guimarães <thaynaraelinezdsf@gmail.com>
2023-01-19 17:47:12 -0300

    embaralhar as cartas, setar as imagens nas cartas, virar apenas 2 cartas por vez

--- js/game.js
+++ js/game.js
@@ -10,7 +10,55 @@ const personagens = [
     'img8',
     'img9',
     'img10',
+    'img1',
+    'img2',
+    'img3',
+    'img4',
+    'img5',
+    'img6',
+    'img7',
+    'img8',
+    'img9',
+    'img10',
 ];
+
+let primeiraCarta = '';
+let segundaCarta = '';
+
+const compararCartar = () => {
+    const primeiroPersonagem = primeiraCarta.getAttribute('data-personagem');
+    const seundoPersonagem = segundaCarta.getAttribute('data-personagem');
+
+    if(primeiroPersonagem == seundoPersonagem){
+
+        
+    }else {
+        setTimeout(() => {
+            primeiraCarta.classList.remove('revela-carta');
+            segundaCarta.classList.remove('revela-carta');
+        }, 500);
+       
+    }
+    
+}
+
+const revelaCarta = ({target}) => {
+
+    if(target.parentNode.className.includes('revela-carta'))
+    {
+        return
+    }
+
+    if(primeiraCarta == ''){
+        target.parentNode.classList.add('revela-carta');   
+        primeiraCarta = target.parentNode;
+    } else if(segundaCarta == ''){
+        target.parentNode.classList.add('revela-carta'); 
+        segundaCarta = target.parentNode;
+    }
+    
+
+}
 
 const createCard = (personagem) => {
     const card = document.createElement('div');
@@ -26,11 +74,15 @@ const createCard = (personagem) => {
     card.appendChild(front);
     card.appendChild(back);
 
+    card.addEventListener('click', revelaCarta)
+    card.setAttribute('data-personagem', personagem);
+
     return card;
 }
 
 const carregarJogo = () => {
-    personagens.forEach((personagem) => {
+    const embaralharCartas= personagens.sort(() => Math.random() - 0.5);
+    embaralharCartas.forEach((personagem) => {
         const card = createCard(personagem);
         grid.appendChild(card)
     })
